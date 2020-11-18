e7ce99c15d6cac1822c396834a41b0617190b8c5
Ondrej Zdych <ondrej.zdych@firma.seznam.cz>
2020-11-18 16:52:56 +0100

    refactor: use NASM macro for expanding ISRs

--- cpu/interrupt.asm
+++ cpu/interrupt.asm
@@ -1,8 +1,30 @@
 ; Defined in isr.c
 [extern isr_handler]
 
+; We don't get information about which interrupt was called
+; when the handler is run, so we will need to have a different handler
+; for every interrupt.
+; Furthermore, some interrupts push an error code onto the stack but others
+; don't, so we will push a dummy error code for those which don't, so that
+; we have a consistent stack for all of them.
+
+%macro no_error_code_interrupt_handler 1
+  global isr%1 ; First make the ISRs global
+  isr%1:
+  push dword 0 ; push 0 as error code
+  push dword %1 ; push the interrupt number
+  jmp common_interrupt_handler ; jump to the common handler
+%endmacro
+
+%macro error_code_interrupt_handler 1
+  global isr%1 ; First make the ISRs global
+  isr%1:
+  push dword %1 ; push the interrupt number
+  jmp common_interrupt_handler  ; jump to the common handler
+%endmacro
+
 ; Common ISR code
-isr_common_stub:
+common_interrupt_handler:
   ; 1. Save CPU state
 	pusha ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
 	mov ax, ds ; Lower 16-bits of eax = ds.
@@ -27,261 +49,67 @@ isr_common_stub:
 	sti
 	iret ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
 
-; We don't get information about which interrupt was called
-; when the handler is run, so we will need to have a different handler
-; for every interrupt.
-; Furthermore, some interrupts push an error code onto the stack but others
-; don't, so we will push a dummy error code for those which don't, so that
-; we have a consistent stack for all of them.
-
-; First make the ISRs global
-global isr0
-global isr1
-global isr2
-global isr3
-global isr4
-global isr5
-global isr6
-global isr7
-global isr8
-global isr9
-global isr10
-global isr11
-global isr12
-global isr13
-global isr14
-global isr15
-global isr16
-global isr17
-global isr18
-global isr19
-global isr20
-global isr21
-global isr22
-global isr23
-global isr24
-global isr25
-global isr26
-global isr27
-global isr28
-global isr29
-global isr30
-global isr31
-
 ; 0: Divide By Zero Exception
-isr0:
-    cli
-    push byte 0
-    push byte 0
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 0
 ; 1: Debug Exception
-isr1:
-    cli
-    push byte 0
-    push byte 1
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 1
 ; 2: Non Maskable Interrupt Exception
-isr2:
-    cli
-    push byte 0
-    push byte 2
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 2
 ; 3: Int 3 Exception
-isr3:
-    cli
-    push byte 0
-    push byte 3
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 3
 ; 4: INTO Exception
-isr4:
-    cli
-    push byte 0
-    push byte 4
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 4
 ; 5: Out of Bounds Exception
-isr5:
-    cli
-    push byte 0
-    push byte 5
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 5
 ; 6: Invalid Opcode Exception
-isr6:
-    cli
-    push byte 0
-    push byte 6
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 6
 ; 7: Coprocessor Not Available Exception
-isr7:
-    cli
-    push byte 0
-    push byte 7
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 7
 ; 8: Double Fault Exception (With Error Code!)
-isr8:
-    cli
-    push byte 8
-    jmp isr_common_stub
-
+error_code_interrupt_handler              8
 ; 9: Coprocessor Segment Overrun Exception
-isr9:
-    cli
-    push byte 0
-    push byte 9
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 9
 ; 10: Bad TSS Exception (With Error Code!)
-isr10:
-    cli
-    push byte 10
-    jmp isr_common_stub
-
+error_code_interrupt_handler              10
 ; 11: Segment Not Present Exception (With Error Code!)
-isr11:
-    cli
-    push byte 11
-    jmp isr_common_stub
-
+error_code_interrupt_handler              11
 ; 12: Stack Fault Exception (With Error Code!)
-isr12:
-    cli
-    push byte 12
-    jmp isr_common_stub
-
+error_code_interrupt_handler              12
 ; 13: General Protection Fault Exception (With Error Code!)
-isr13:
-    cli
-    push byte 13
-    jmp isr_common_stub
-
+error_code_interrupt_handler              13
 ; 14: Page Fault Exception (With Error Code!)
-isr14:
-    cli
-    push byte 14
-    jmp isr_common_stub
-
+error_code_interrupt_handler              14
 ; 15: Reserved Exception
-isr15:
-    cli
-    push byte 0
-    push byte 15
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 15
 ; 16: Floating Point Exception
-isr16:
-    cli
-    push byte 0
-    push byte 16
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 16
 ; 17: Alignment Check Exception
-isr17:
-    cli
-    push byte 0
-    push byte 17
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 17
 ; 18: Machine Check Exception
-isr18:
-    cli
-    push byte 0
-    push byte 18
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 18
 ; 19: Reserved
-isr19:
-    cli
-    push byte 0
-    push byte 19
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 19
 ; 20: Reserved
-isr20:
-    cli
-    push byte 0
-    push byte 20
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 20
 ; 21: Reserved
-isr21:
-    cli
-    push byte 0
-    push byte 21
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 21
 ; 22: Reserved
-isr22:
-    cli
-    push byte 0
-    push byte 22
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 22
 ; 23: Reserved
-isr23:
-    cli
-    push byte 0
-    push byte 23
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 23
 ; 24: Reserved
-isr24:
-    cli
-    push byte 0
-    push byte 24
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 24
 ; 25: Reserved
-isr25:
-    cli
-    push byte 0
-    push byte 25
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 25
 ; 26: Reserved
-isr26:
-    cli
-    push byte 0
-    push byte 26
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 26
 ; 27: Reserved
-isr27:
-    cli
-    push byte 0
-    push byte 27
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 27
 ; 28: Reserved
-isr28:
-    cli
-    push byte 0
-    push byte 28
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 28
 ; 29: Reserved
-isr29:
-    cli
-    push byte 0
-    push byte 29
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 29
 ; 30: Reserved
-isr30:
-    cli
-    push byte 0
-    push byte 30
-    jmp isr_common_stub
-
+no_error_code_interrupt_handler 30
 ; 31: Reserved
-isr31:
-    cli
-    push byte 0
-    push byte 31
-    jmp isr_common_stub
+no_error_code_interrupt_handler 31
