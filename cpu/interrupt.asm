ce76901fcd039bf879a42679a972517966d53878
Ondrej Zdych <ondrej.zdych@firma.seznam.cz>
2020-11-18 21:11:54 +0100

    feat: implement PIC driver

--- cpu/interrupt.asm
+++ cpu/interrupt.asm
@@ -1,5 +1,6 @@
 ; Defined in isr.c
 [extern isr_handler]
+[extern irq_handler]
 
 ; We don't get information about which interrupt was called
 ; when the handler is run, so we will need to have a different handler
@@ -11,20 +12,29 @@
 %macro no_error_code_interrupt_handler 1
   global isr%1 ; First make the ISRs global
   isr%1:
-  push dword 0 ; push 0 as error code
-  push dword %1 ; push the interrupt number
-  jmp common_interrupt_handler ; jump to the common handler
+    push dword 0 ; push 0 as error code
+    push dword %1 ; push the interrupt number
+    jmp isr_common_stub ; jump to the common handler
 %endmacro
 
 %macro error_code_interrupt_handler 1
   global isr%1 ; First make the ISRs global
   isr%1:
-  push dword %1 ; push the interrupt number
-  jmp common_interrupt_handler  ; jump to the common handler
+    push dword %1 ; push the interrupt number
+    jmp isr_common_stub  ; jump to the common handler
+%endmacro
+
+%macro interrupt_request_handler 2
+  global irq%1
+  irq%1:
+    cli
+    push dword %1
+    push dword %2
+    jmp irq_common_stub
 %endmacro
 
 ; Common ISR code
-common_interrupt_handler:
+isr_common_stub:
   ; 1. Save CPU state
 	pusha ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
 	mov ax, ds ; Lower 16-bits of eax = ds.
@@ -48,6 +58,30 @@ common_interrupt_handler:
 	add esp, 8 ; Cleans up the pushed error code and pushed ISR number
 	sti
 	iret ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
+
+; Common IRQ code. Identical to ISR code except for the 'call'
+; and the 'pop ebx'
+irq_common_stub:
+  pusha
+  mov ax, ds
+  push eax
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+
+  call irq_handler ; Different than the ISR code
+
+  pop ebx  ; Different than the ISR code
+  mov ds, bx
+  mov es, bx
+  mov fs, bx
+  mov gs, bx
+  popa
+  add esp, 8
+  sti
+  iret
 
 ; 0: Divide By Zero Exception
 no_error_code_interrupt_handler 0
@@ -113,3 +147,21 @@ no_error_code_interrupt_handler 29
 no_error_code_interrupt_handler 30
 ; 31: Reserved
 no_error_code_interrupt_handler 31
+
+; IRQ handlers
+interrupt_request_handler 0, 32
+interrupt_request_handler 1, 33
+interrupt_request_handler 2, 34
+interrupt_request_handler 3, 35
+interrupt_request_handler 4, 36
+interrupt_request_handler 5, 37
+interrupt_request_handler 6, 38
+interrupt_request_handler 7, 39
+interrupt_request_handler 8, 40
+interrupt_request_handler 9, 41
+interrupt_request_handler 10, 42
+interrupt_request_handler 11, 43
+interrupt_request_handler 12, 44
+interrupt_request_handler 13, 45
+interrupt_request_handler 14, 46
+interrupt_request_handler 15, 47
