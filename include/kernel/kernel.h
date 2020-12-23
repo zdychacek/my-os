4bb40f832026df8ca7bca2139318a2a7a5df627d
Ondrej Zdych <ondrej.zdych@firma.seznam.cz>
2020-12-23 15:59:43 +0100

    feat: make library target

--- include/kernel/kernel.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void user_input(char *input);
---
+++ src/kernel/hal/pic.c
@@ -0,0 +1,73 @@
+#include "kernel/hal/pic.h"
+#include "kernel/hal/ports.h"
+
+/*
+arguments:
+	offset1 - vector offset for master PIC
+		vectors on the master become offset1..offset1+7
+	offset2 - same for slave PIC: offset2..offset2+7
+*/
+void PIC_remap(int offset1, int offset2)
+{
+  // save masks
+  uint8_t a1 = port_byte_read(PIC1_DATA);
+  uint8_t a2 = port_byte_read(PIC2_DATA);
+
+  // starts the initialization sequence (in cascade mode)
+  port_byte_write(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+  port_byte_write(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+  port_byte_write(PIC1_DATA, offset1); // ICW2: Master PIC vector offset
+  port_byte_write(PIC2_DATA, offset2); // ICW2: Slave PIC vector offset
+  port_byte_write(PIC1_DATA, 4);       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+  port_byte_write(PIC2_DATA, 2);       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+  port_byte_write(PIC1_DATA, ICW4_8086);
+  port_byte_write(PIC2_DATA, ICW4_8086);
+
+  // restore saved masks
+  port_byte_write(PIC1_DATA, a1);
+  port_byte_write(PIC2_DATA, a2);
+}
+
+void PIC_sendEOI(uint8_t int_num)
+{
+  if (int_num >= 40)
+    port_byte_write(PIC2_COMMAND, PIC_EOI);
+
+  port_byte_write(PIC1_COMMAND, PIC_EOI);
+}
+
+void IRQ_set_mask(uint8_t IRQline)
+{
+  uint16_t port;
+  uint8_t value;
+
+  if (IRQline < 8)
+  {
+    port = PIC1_DATA;
+  }
+  else
+  {
+    port = PIC2_DATA;
+    IRQline -= 8;
+  }
+  value = port_byte_read(port) | (1 << IRQline);
+  port_byte_write(port, value);
+}
+
+void IRQ_clear_mask(uint8_t IRQline)
+{
+  uint16_t port;
+  uint8_t value;
+
+  if (IRQline < 8)
+  {
+    port = PIC1_DATA;
+  }
+  else
+  {
+    port = PIC2_DATA;
+    IRQline -= 8;
+  }
+  value = port_byte_read(port) & ~(1 << IRQline);
+  port_byte_write(port, value);
+}
